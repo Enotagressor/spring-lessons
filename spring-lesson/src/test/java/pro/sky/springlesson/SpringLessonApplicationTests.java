ab250fea24d65fa6d627e21609690d3943ad5f20
Sergey <enotagressor@gmail.com>
2022-06-18 03:15:20 +0700

    1

---
+++ spring-lesson/src/test/java/pro/sky/springlesson/SpringLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.springlesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
